bac95ef82f98db515c3b33b63f2e29080c3bb794
winchesterdeeplay <Winchester@deeplay.io>
2023-02-02 07:23:10 +0800

    Finalize project

--- data_generator_executor.py
+++ data_generator_executor.py
@@ -1,17 +1,19 @@
+import logging
+import random
 from os.path import join
 from pathlib import Path
 
 import keyring
+import numpy as np
 
 from lib.clickhouse_hook import ClickhouseNativeHook
 from lib.data_generator import ShopDataGenerator
 from lib.load_query import load_query
-import logging
-import random
 
-HOST = "ch_server"  # change to localhost
-# HOST = "localhost"
+# HOST = "ch_server"  # change to localhost
+HOST = "localhost"
 GENERATE_DATA = True
+TRUNCATE_ON_START = True
 QUERIES_PATH = "queries"
 DIR_PATH = Path(__file__).parent.resolve()
 SEED = 42
@@ -35,12 +37,18 @@ CLICKHOUSE_LOGIN = keyring.get_password("clickhouse_lab1", "username")
 CLICKHOUSE_PASSWORD = keyring.get_password("clickhouse_lab1", "password")
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     random.seed(SEED)
+    np.random.seed(SEED)
     client = ClickhouseNativeHook(login=CLICKHOUSE_LOGIN, password=CLICKHOUSE_PASSWORD, host=HOST).get_connection()
     # create tables if not exists
     for entity in ENTITIES:
         query = load_query(join(DIR_PATH, join(QUERIES_PATH, f"{entity}.sql")))
         client.execute(query=query)
+        if TRUNCATE_ON_START and entity != "shop":
+            truncate_query = f"TRUNCATE TABLE shop.{entity}"
+            logging.info(truncate_query)
+            client.execute(query=truncate_query)
 
     logging.info("Tables created successfully.")
     # generate data
--- lib/data_generator.py
+++ lib/data_generator.py
@@ -45,7 +45,7 @@ SELECT if({new_name!r} IN Names, 1, 0) AS Result"""
         return self.client.execute(query=query)[0][0]
 
     @staticmethod
-    def generate_random_uint_value(min_value: int = 0, max_value: int = 1000) -> int:
+    def generate_random_uint_value(min_value: int = 0, max_value: int = 2000) -> int:
         return randrange(min_value, max_value)
 
     @staticmethod
@@ -62,8 +62,8 @@ SELECT if({new_name!r} IN Names, 1, 0) AS Result"""
 
 
 class ShopDataGenerator(DataGeneratorClickhouse):
-    PROVIDER_NAMES = ["China", "Russia", "USA", "Canada", "EU", "Africa", "Thailand", "Australia"]
-    STORAGE_NAMES = ["Warehouse", "Shop", "Secret"]
+    PROVIDER_NAMES = ["China", "Russia", "USA", "Canada", "EU", "Africa", "Thailand", "Australia", "Georgia"]
+    STORAGE_NAMES = ["Warehouse", "Shop", "Secret", "Home", "Virtual"]
     ADDRESSES_NAMES = ["Vladivostok", "Moscow", "Petersburg", "Yekaterinburg", "Yakutsk", "Sakhalin"]
     PRODUCT_NAMES = ["Milk", "Computer", "Tea", "Coffee", "Sweet", "Beer", "Wear"]
 
@@ -72,12 +72,12 @@ class ShopDataGenerator(DataGeneratorClickhouse):
         insert_query="INSERT INTO {table} VALUES",
         select_id_query="SELECT {id_field_name} FROM {table}",
         id_field_name: str = "ID",
-        provider_count: int = 500,
-        storage_count: int = 500,
-        delivery_count: int = 1000,
-        product_count: int = 1000,
-        delivery_products_line_count: int = 700,
-        invoice_product_line_count: int = 300,
+        provider_count: int = 50,
+        storage_count: int = 50,
+        delivery_count: int = 100,
+        product_count: int = 100,
+        delivery_products_line_count: int = 70,
+        invoice_product_line_count: int = 50,
         provider_table_name: str = "shop.provider",
         provider_name_field_name: str = "Name",
         storage_table_name: str = "shop.storage",
@@ -101,6 +101,7 @@ class ShopDataGenerator(DataGeneratorClickhouse):
         invoice_table_name: str = "shop.invoice",
         invoice_storage_id_field_name: str = "StorageID",
         invoice_invoice_date_time_field_name: str = "InvoiceDateTime",
+        logger: Any = None,
         **kwargs,
     ):
         """
@@ -158,6 +159,12 @@ class ShopDataGenerator(DataGeneratorClickhouse):
         self.invoice_storage_id_field_name = invoice_storage_id_field_name
         self.invoice_invoice_date_time_field_name = invoice_invoice_date_time_field_name
 
+        self.logger = logger if logger is not None else logging.getLogger()
+
+        self.products_cache = None
+        self.product_cache = None
+        self.delivery_cache = None
+
     def generate_provider_row(self, to_insert: bool = True) -> Provider:
         name_available_state = 1
         new_name = ""
@@ -167,6 +174,8 @@ class ShopDataGenerator(DataGeneratorClickhouse):
             name_available_state = self.check_name_value_available(
                 new_name, self.provider_name_field_name, self.provider_table_name
             )
+            if name_available_state and self.logger:
+                self.logger.warning(f"Попытка добавить существующего провайдера, отказано. {new_name}")
         provider = Provider(ID=new_id, Name=new_name)
         if to_insert:
             self._insert_record(asdict(provider), self.provider_table_name)
@@ -183,6 +192,8 @@ class ShopDataGenerator(DataGeneratorClickhouse):
             name_available_state = self.check_name_value_available(
                 new_name, self.storage_name_field_name, self.storage_table_name
             )
+            if name_available_state and self.logger:
+                self.logger.warning(f"Попытка добавить существующий склад, отказано. {new_name}")
 
         while address_available_state:
             new_address = f"{choice(self.ADDRESSES_NAMES)}_{self.generate_random_uint_value()}"
@@ -225,12 +236,16 @@ class ShopDataGenerator(DataGeneratorClickhouse):
             code_available_state = self.check_name_value_available(
                 new_name, self.product_code_field_name, self.product_table_name
             )
+            if code_available_state and self.logger:
+                self.logger.warning(f"Попытка добавить товар с неуникальным кодом, отказано. {new_code}")
         product = Product(ID=new_id, Name=new_name, Code=new_code)
         if to_insert:
             self._insert_record(asdict(product), self.product_table_name)
         return product
 
-    def generate_delivery_product_line_row(self, to_insert: bool = True) -> DeliveryProductsLine:
+    def generate_delivery_product_line_row(
+        self, to_insert: bool = True, min_quantity: int = 1, max_quantity: int = 100
+    ) -> DeliveryProductsLine:
         new_id = self.generate_next_uint_id(
             table_name=self.delivery_products_line_table_name, id_field_name=self.id_field_name
         )
@@ -240,13 +255,18 @@ class ShopDataGenerator(DataGeneratorClickhouse):
         select_delivery_query = self.select_id_query.format(
             id_field_name=self.id_field_name, table=self.delivery_table_name
         )
-
-        products = self.client.query_dataframe(select_product_query)
-        deliveries = self.client.query_dataframe(select_delivery_query)
+        if self.product_cache is None and self.delivery_cache is None:
+            products = self.client.query_dataframe(select_product_query)
+            deliveries = self.client.query_dataframe(select_delivery_query)
+            self.product_cache = products
+            self.delivery_cache = deliveries
+        else:
+            products = self.product_cache
+            deliveries = self.delivery_cache
 
         product, delivery = products.sample(n=1)[self.id_field_name], deliveries.sample(n=1)[self.id_field_name]
 
-        quantity = self.generate_random_uint_value(min_value=1, max_value=100)
+        quantity = self.generate_random_uint_value(min_value=min_quantity, max_value=max_quantity)
 
         delivery_products_line = DeliveryProductsLine(
             ID=new_id, ProductID=product, DeliveryID=delivery, Quantity=quantity
@@ -272,7 +292,11 @@ class ShopDataGenerator(DataGeneratorClickhouse):
             id_field_name=self.delivery_products_line_product_id_field_name,
             table=self.delivery_products_line_table_name,
         )
-        products = self.client.query_dataframe(select_product_query)
+        if self.products_cache is None:
+            products = self.client.query_dataframe(select_product_query)
+            self.products_cache = products
+        else:
+            products = self.products_cache
         product = int(products.sample(n=1)[self.delivery_products_line_product_id_field_name])
 
         delivery_quantity_sum_by_storage_id_query = f"""SELECT 
@@ -320,14 +344,31 @@ GROUP BY StorageID;"""
         invoice_quantity = self.client.query_dataframe(query=invoice_quantity_sum_by_storage_id_query)
         if invoice_quantity.empty:
             max_value = delivery_quantity.sort_values("QuantitySum", ascending=False).iloc[0, :]
-            quantity = self.generate_random_uint_value(min_value=1, max_value=int(max_value["QuantitySum"]))
+            quantity = self.generate_random_uint_value(min_value=1, max_value=int(max_value["QuantitySum"] * 1.5))
+            if quantity > int(max_value["QuantitySum"]):
+                if self.logger:
+                    self.logger.warning(
+                        f"Попытка сделать заказ большего количества чем есть, отказано. {quantity} > {int(max_value['QuantitySum'])}"
+                    )
+                return
         else:
             merged = pd.merge(delivery_quantity, invoice_quantity, on="StorageID", suffixes=("_x", "_y"))
             merged["QuantityDiff"] = merged["QuantitySum_x"] - merged["QuantitySum_y"]
             max_value = merged.sort_values("QuantityDiff", ascending=False).iloc[0, :]
             if int(max_value["QuantityDiff"]) < 0:
-                return None
-            quantity = self.generate_random_uint_value(min_value=1, max_value=int(max_value["QuantityDiff"]))
+                if self.logger:
+                    self.logger.error("Ошибка оформления заказа")
+                return
+            if int(max_value["QuantityDiff"]) == 1:
+                quantity = 1
+            else:
+                quantity = self.generate_random_uint_value(min_value=1, max_value=int(max_value["QuantityDiff"] * 1.5))
+                if quantity > int(max_value["QuantityDiff"]):
+                    if self.logger:
+                        self.logger.warning(
+                            f"Попытка сделать заказ большего количества чем есть, отказано. {quantity} > {int(max_value['QuantityDiff'])}"
+                        )
+                    return
         storage_id = int(max_value["StorageID"])
         min_datetime = pd.to_datetime(max_value["DeliveryDateTimeMin"]).strftime(self.DATETIME_FORMAT)
         invoice_datetime = self.generate_random_datetime(min_value=min_datetime, time_format=self.DATETIME_FORMAT)
@@ -345,22 +386,22 @@ GROUP BY StorageID;"""
     def execute(self) -> None:
         for _ in range(self.provider_count):
             self.generate_provider_row()
-        logging.info("Providers successfully inserted.")
+        self.logger.info("Providers successfully inserted.")
         for _ in range(self.storage_count):
             self.generate_storage_row()
-        logging.info("Storages successfully inserted.")
+        self.logger.info("Storages successfully inserted.")
         for _ in range(self.delivery_count):
             self.generate_delivery_row()
-        logging.info("Deliveries successfully inserted.")
+        self.logger.info("Deliveries successfully inserted.")
         for _ in range(self.product_count):
             self.generate_product_row()
-        logging.info("Products successfully inserted.")
+        self.logger.info("Products successfully inserted.")
         for _ in range(self.delivery_products_line_count):
             self.generate_delivery_product_line_row()
-        logging.info("Delivery_product_lines successfully inserted.")
+        self.logger.info("Delivery_product_lines successfully inserted.")
         for _ in range(self.invoice_product_line_count):
             self.generate_invoice__invoice_product_line_rows()
-        logging.info("Invoices successfully inserted.")
+        self.logger.info("Invoices successfully inserted.")
 
     def _insert_record(self, record: Dict[str, Any], table_name: str) -> None:
         self.client.insert_dataframe(
